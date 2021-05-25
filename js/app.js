eea884a842addd4a41a0b5f718a32624c3bd3e8d
Mahmoudalsefarini <mahmoudalsafarini@gmail.com>
2021-05-25 09:23:45 +0300

    html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let travil = prompt ('Do you think i like to travil ?')
+let age = prompt ('Do you think im older than 28 years ?')
+let home = prompt ('Do you think i live in amman ?')
+let tall = prompt ('Do you think im taller than 180 cm ?')
