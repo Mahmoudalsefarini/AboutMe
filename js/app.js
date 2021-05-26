7769c1947a43f7f22f16ab2e952e064f9de25f24
Mahmoudalsefarini <mahmoudalsafarini@gmail.com>
2021-05-26 09:58:34 +0300

    lab3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,84 @@
 'use strict';
+let travil = prompt('Do you think i like to travil ?');
+switch (travil.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log('thats right i do like traviling');  
+        alert('thats right i do like traviling')
+break;
+     case 'no':
+     case 'n':
+       //console.log('thats not right! travil is the life')
+       alert('thats not right! travil is the life');}
+let age = prompt('Do you think im older than 27 years ?');
+switch (age.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log('thats not right , im 26');  
+        alert('thats not right , im 26')
+        break;
+    case 'no':
+    case 'n':
+        //console.log('thats right ')
+        alert('thats right');
+}
+let home = prompt('Do you think i live in amman ?');
+switch (home.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log('thats right , i live in Amman ');  
+        alert('thats right , i live in Amman')
+        break;
+    case 'no':
+    case 'n':
+        //console.log('thats not right!')
+        alert('thats not right!');
+}
+let tall = prompt('Do you think im taller than 180 cm ?');
+switch (tall.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log('thats right');  
+        alert('thats right ')
+        break;
+    case 'no':
+    case 'n':
+        //console.log('thats not right!')
+        alert('thats not right!');
+}
+let major = prompt('Do you think my major is Computer science ?');
+switch (major.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log('thats not right');  
+        alert('thats not right ')
+        break;
+    case 'no':
+    case 'n':
+        //console.log('thats right < my major is civil enginner !')
+        alert('thats right < my major is civil enginner !');
+}
 
-let travil = prompt ('Do you think i like to travil ?')
-let age = prompt ('Do you think im older than 28 years ?')
-let home = prompt ('Do you think i live in amman ?')
-let tall = prompt ('Do you think im taller than 180 cm ?')
+
+let score = 0;
+
+for (let x = 0; x < 4; x++){
+
+    let guessNumber = prompt('guess a number How far is LTUC from my home in km')
+    guessNumber = Number(guessNumber);
+
+    if(guessNumber ===30){
+        score++;
+        alert('The number is correct');
+        break;
+    }else if (guessNumber<30){
+        alert('you are too low , try again')
+    }else if(guessNumber>30){
+        alert('you are too high , try again')
+    }if (x===3){
+        alert('you lost , the answer is 30')
+    }
+}
+
+
+
