79209558ebab607d16f0cff003fefc0947c3b4c4
Mahmoudalsefarini <mahmoudalsafarini@gmail.com>
2021-05-26 10:59:10 +0300

    Adding Q7

--- js/app.js
+++ js/app.js
@@ -5,11 +5,12 @@ switch (travil.toLowerCase()) {
     case 'y':
         //console.log('thats right i do like traviling');  
         alert('thats right i do like traviling')
-break;
-     case 'no':
-     case 'n':
-       //console.log('thats not right! travil is the life')
-       alert('thats not right! travil is the life');}
+        break;
+    case 'no':
+    case 'n':
+        //console.log('thats not right! travil is the life')
+        alert('thats not right! travil is the life');
+}
 let age = prompt('Do you think im older than 27 years ?');
 switch (age.toLowerCase()) {
     case 'yes':
@@ -61,24 +62,36 @@ switch (major.toLowerCase()) {
 
 
 let score = 0;
-
-for (let x = 0; x < 4; x++){
+let guessNumber = null;
+for (let x = 0; x < 4; x++) {
 
     let guessNumber = prompt('guess a number How far is LTUC from my home in km')
     guessNumber = Number(guessNumber);
 
-    if(guessNumber ===30){
+    if (guessNumber === 30) {
         score++;
         alert('The number is correct');
         break;
-    }else if (guessNumber<30){
+    } else if (guessNumber < 30) {
         alert('you are too low , try again')
-    }else if(guessNumber>30){
+    } else if (guessNumber > 30) {
         alert('you are too high , try again')
-    }if (x===3){
-        alert('you lost , the answer is 30')
+    } if (x === 3) {
+        alert('you lost , the answer is 30');
     }
 }
 
+let favCity = ['newyork', 'chicago', 'boston', 'maine'];
 
-
+for (let i = 0; i < 6; i++) {
+    let user = prompt('Guess one of my favourite city !');
+    if (favCity[0] === user || favCity[1] === user || favCity[2] === user || favCity[4] === user) {
+        alert('correct answer');
+        score++
+        break;
+    }
+    if (i === 4) {
+        alert('you lost , the answer is (newyork, chicago , boston , maine) ');
+        break;
+    }
+}
